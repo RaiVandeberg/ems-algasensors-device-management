ebdf6168003dc3a3eb5f9ec3579c13d2ed8e7b09
Rai Vandenberg <rvdesigner20@gmail.com>
2025-10-18 19:49:55 -0300

    feat(sensor): add enable, disable and delete operations

--- src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -1,5 +1,4 @@
 package com.algaworks.algasensors.device.management.api.controller;
-
 import com.algaworks.algasensors.device.management.api.model.SensorInput;
 import com.algaworks.algasensors.device.management.api.model.SensorOutput;
 import com.algaworks.algasensors.device.management.common.IdGenerator;
@@ -7,17 +6,20 @@ import com.algaworks.algasensors.device.management.domain.model.Sensor;
 import com.algaworks.algasensors.device.management.domain.model.SensorId;
 import com.algaworks.algasensors.device.management.domain.repository.SensorRepository;
 import io.hypersistence.tsid.TSID;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/sensors")
 @RequiredArgsConstructor
+@Validated
 public class SensorController {
 
     private final SensorRepository sensorRepository;
@@ -29,10 +31,9 @@ public class SensorController {
 
     }
 
-    @GetMapping("{sensorId}")
+    @GetMapping("/{sensorId}")
     public SensorOutput getOne(@PathVariable TSID sensorId) {
-        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        Sensor sensor = getSensorDB(sensorId);
             return convertToModel(sensor);
     }
 
@@ -54,6 +55,43 @@ public class SensorController {
         return convertToModel(sensor);
     }
 
+    @PutMapping("/{sensorId}")
+    @ResponseStatus(HttpStatus.OK)
+    public SensorOutput update(@PathVariable @NotNull TSID sensorId, @RequestBody @Validated SensorOutput input){
+        Sensor sensor = getSensorDB(sensorId);
+        sensor.setName(input.getName());
+        sensor.setIp(input.getIp());
+        sensor.setLocation(input.getLocation());
+        sensor.setProtocol(input.getProtocol());
+        sensor.setModel(input.getModel());
+        sensor = sensorRepository.saveAndFlush(sensor);
+        return convertToModel(sensor);
+    }
+
+
+    @PutMapping("/{sensorId}/enable")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void enable(@PathVariable @NotNull TSID sensorId) {
+        Sensor sensor = getSensorDB(sensorId);
+        sensor.setEnabled(true);
+        sensorRepository.saveAndFlush(sensor);
+    }
+
+    @DeleteMapping("/{sensorId}/enable")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void disable(@PathVariable @NotNull TSID sensorId) {
+        Sensor sensor = getSensorDB(sensorId);
+        sensor.setEnabled(false);
+        sensorRepository.saveAndFlush(sensor);
+    }
+
+    @DeleteMapping("{sensorId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable @NotNull TSID sensorId) {
+        Sensor sensor = getSensorDB(sensorId);
+        sensorRepository.delete(sensor);
+    }
+
     private SensorOutput convertToModel(Sensor sensor) {
         return SensorOutput.builder()
                 .id(sensor.getId().getValue())
@@ -64,6 +102,10 @@ public class SensorController {
                 .model(sensor.getModel())
                 .enabled(sensor.getEnabled())
                 .build();
+    }
+
+    private Sensor getSensorDB(TSID sensorId) {
+        return sensorRepository.findById(new SensorId(sensorId)).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
 }
